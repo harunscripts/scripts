8819b9162c5758aef5dbe97245401037e50772f1
harun <37047409+harunscripts@users.noreply.github.com>
2022-10-23 20:19:21 -0700

    Updated Flaws
    I've updated a big flaw which allowed functions to be executed without proper capitalization. This has been fixed, and I have added so scripts can't force full screen you.

--- AntiFetch.lua
+++ AntiFetch.lua
@@ -2,20 +2,23 @@ local old
 old = hookmetamethod(game, "__namecall", function(self, ...)
     local args = {...}
     local method = getnamecallmethod()
-    if (method == "GetHttpResultHistory" and self == game:GetService("LogService")) and checkcaller() then
+    if (method:lower() == "gethttpresulthistory" and self == game:GetService("LogService")) and checkcaller() then
         return {}
     end
-    if (method == "GetOutputDevices" and self == game:GetService("SoundService")) and checkcaller() then
+    if (method:lower() == "getoutputdevices" and self == game:GetService("SoundService")) and checkcaller() then
         return {}
     end
-    if (method == "GetCameraDevices" and self == game:GetService("VideoCaptureService")) and checkcaller() then
+    if (method:lower() == "getcameradevices" and self == game:GetService("VideoCaptureService")) and checkcaller() then
         return {}
     end
-    if (method == "GetClientId" and self == game:GetService("RbxAnalyticsService")) and checkcaller() then
+    if (method:lower() == "getclientid" and self == game:GetService("RbxAnalyticsService")) and checkcaller() then
         return game:GetService("HttpService"):GenerateGUID(false):upper()
     end
-    if (method == "GetUserAgent" and self == game:GetService("HttpService")) and checkcaller() then
+    if (method:lower() == "getuseragent" and self == game:GetService("HttpService")) and checkcaller() then
         return ""
+    end
+    if (method:lower() == "togglefullscreen" and self == game:GetService("GuiService")) and checkcaller() then
+        return nil
     end
     return old(self, unpack(args))
 end)
